6dba752cda40a9ff231ec7c2cbb52cea384a73d4
tyamzak <getagelessgetageless@gmail.com>
2022-07-04 07:33:32 +0900

    重複の処理追加

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ from slack_sdk.errors import SlackApiError
 from logging import getLogger, StreamHandler, DEBUG
 import datetime
 import json
+STARTTIME = time.time()
 
-with open('config.json','r') as f:
+
+with open('config.json','r',encoding='utf8') as f:
     js = json.load(f)
     slack_token = js["SLACK_TOKEN"]
+    SLACK_IRAISYO_STR = js["SLACK_IRAISYO_STR"]
+    SLACK_FEEDBACK_STR = js["SLACK_FEEDBACK_STR"]
     BOX_USER_ID = js["BOX_USER_ID"]
     SLACK_CHANNEL_NAMES = js["SLACK_CHANNEL_NAMES"]
-    TIMEOUT = float(js["TIMEOUT"])   
-
+    TIMEOUT = float(js["TIMEOUT"])
 
 def get_tmp_folder():
     SAVEFOLDER = "/tmp"
@@ -32,12 +35,11 @@ def get_tmp_folder():
         SAVEFOLDER = "/tmp"
     return SAVEFOLDER
 
-STARTTIME = time.time()
 if not TIMEOUT:
     TIMEOUT = 450
 else:
     TIMEOUT = int(TIMEOUT)
-
+    
 logging.basicConfig()
 logger = getLogger(__name__)
 handler = logging.StreamHandler(sys.stdout)
@@ -93,8 +95,11 @@ def slack_filelist_for_download(channels:list[str]=[],
     file_ids = []
     global slack_client
     
+    
     if channels:
         for channel in channels:
+            #チャンネルの下で重複チェックを行う
+            names_set = set()
             for page in slack_client.files_list(channel=channel,ts_from=ts_from, ts_to=ts_to,page=page):
                 for file in page.get("files", []):
                     info = {
@@ -105,6 +110,27 @@ def slack_filelist_for_download(channels:list[str]=[],
                             "download_url" : file["url_private_download"],
                             "timestamp" : file["timestamp"]
                         }
+                    #同日付で同名ファイルがあった場合に、リネームする(1).(2)形式
+                    #setはチャンネル + 日付 + ファイル名形式
+                    
+
+                    # #既に存在したら、(1)形式でファイル名に追加
+                    i = 0
+                    channelstr = info['channel_id'][0]
+                    datestr = datetime.datetime.fromtimestamp(info["timestamp"],tz=JST).strftime(format='%Y%m%d')
+                    keystr = info["file_name"]
+                    datekeyname = channelstr + datestr + keystr
+
+                    while True:
+                        i += 1
+                        if datekeyname in names_set:
+                            str = info["file_name"]
+                            keystr =str.split('.')[0] + f'({i}).' + str.split('.')[1]
+                            datekeyname = channelstr + datestr + keystr
+                        else:
+                            info["file_name"] = keystr
+                            break
+                    names_set.add(datekeyname)
                     file_ids.append(info)
                 #ページがまだあるなら、再帰する
                 paging = page.get("paging")
@@ -161,7 +187,7 @@ def get_slack_channel_ids_names(channel_names:list):
 
 
     for chname in slack_channel_names:
-        if chname['name'] in SLACK_CHANNEL_NAMES and chname["is_channel"]:
+        if chname['name'] in channel_names and chname["is_channel"]:
             local_slack_ids_names.append([chname['name'],chname['id']])
 
     return local_slack_ids_names
@@ -210,57 +236,201 @@ def get_items_from_box_folder(channel_folder_name:str,date_folder_name:str="",ro
     return box_items
 ###################################boxファイルのリストアップ##################################
 
-#mainエリア
+def get_channel_messages(channel_ids) -> list:
+    
+    global TIMEOUT
+    global STARTTIME
+    global slack_client
+    
+    rtnmessages = []
 
+    for channel_id in channel_ids:
+        cursor = None  # シーク位置。最初は None ページを指定して、次からは next_cursor が指し示す位置。
+
+        while True:
+            try:
+                response = slack_client.conversations_history(  # conversations_history ＝ チャット一覧を得る
+                    channel=channel_id,
+                    cursor=cursor  # チャンネルID、latest、シーク位置を指定。
+                    # latestに指定した時間よりも古いメッセージが得られる。latestはUNIX時間で指定する。
+                )
+            except SlackApiError as e:
+                exit
+
+            # response["messages"]が有るか？
+            if "messages" in response:  
+                #チャンネル情報を追加
+                extenditems = []
+                for item in response["messages"]:
+                    item['channel_id'] = channel_id
+                # rtnmessages[-1]['channel_id'] = channel_id
+                # extenditems = [item['channel_id'] = channel_id for item in response["messages"]]
+                rtnmessages.extend(response["messages"])
+                # rtnmessages.extend(extenditems)
+
+
+            if "has_more" not in response or response["has_more"] is not True:
+                # conversations_historyのレスポンスに["has_more"]が無かったり、has_moreの値がFalseだった場合、終了する。
+                break
+            # conversations_historyのレスポンスに["response_metadata"]["next_cursor"]が有る場合、cursorをセット
+            if (
+                "response_metadata" in response
+                and "next_cursor" in response["response_metadata"]
+            ):  
+                # （上に戻って、もう一度、conversations_history取得）
+                cursor = response["response_metadata"]["next_cursor"]
+            else:
+                break
+            
+            #Timeout
+            if time.time() - STARTTIME > TIMEOUT:
+                logger.info("TIMEOUT")
+                break
+    
+    return rtnmessages
+
+    #mainエリア
 
 slack_ids_names = get_slack_channel_ids_names(SLACK_CHANNEL_NAMES)
 channel_ids = [row[1] for row in slack_ids_names]
 #SLACKからダウンロード候補リストを取得する
 file_ids = slack_filelist_for_download(channels = channel_ids, ts_to = TS_TODAY, ts_from = 0)
+slack_channel_messages = get_channel_messages(channel_ids)
+
+#依頼書の格納リスト
+
+iraisyolist = []
+# if not SLACK_IRAISYO_STR:
+#     SLACK_IRAISYO_STR = '*依頼書*を送信しました'
 
 
+#フィードバックの格納リスト
+feedbacklist = []
+#フィードバック認識する文字列
+# if not SLACK_FEEDBACK_STR:
+#     SLACK_FEEDBACK_STR = '*フィードバック*を送信しました' 
 
-    
+for message in slack_channel_messages:
+
+    if SLACK_IRAISYO_STR in message['text']:
+        #認識文字列の検索　=> 項目検索中　=>　項目の値取得中　=>　項目検索中
+        state = 0
+        key = ''
+        value = ''
+        dictforiraicsv = dict()
+        #チャンネル名とスレッド日時の入力
+        message_channel_id = message['channel_id']
+        message_channel_name =  [ row[0] for row in slack_ids_names if row[1] == message_channel_id ][0]
+        dictforiraicsv["チャンネル名"] = message_channel_name
+        dictforiraicsv["スレッド日時"] = datetime.datetime.fromtimestamp(float(message['ts']),tz=JST).isoformat()
+
+        #項目の取得
+        for line in message['text'].split("\n"):
+            #認識文字列の検索
+            if state == 0:
+                if SLACK_IRAISYO_STR in line:
+                    state = 1
+            #項目検索中
+            elif state == 1:
+                if line != '':
+                    key = line
+                    dictforiraicsv[key] =""
+                    state = 2
+            #項目の値取得中
+            elif state == 2:
+                if line != '':
+                    dictforiraicsv[key] = dictforiraicsv[key] + line
+                else:
+                    key = ''
+                    value = ''
+                    state = 1
+        iraisyolist.append(dictforiraicsv)
+
+    if SLACK_FEEDBACK_STR in message['text']:
+        #認識文字列の検索　=> 項目検索中　=>　項目の値取得中　=>　項目検索中
+        state = 0
+        key = ''
+        value = ''
+        dictforfeedbackcsv = dict()
+        dictforfeedbackcsv["チャンネル名"] = message_channel_name
+        dictforfeedbackcsv["スレッド日時"] = datetime.datetime.fromtimestamp(float(message['ts']),tz=JST).isoformat()
+
+        #項目の取得
+        for line in message['text'].split("\n"):
+            #認識文字列の検索
+            if state == 0:
+                if SLACK_FEEDBACK_STR in line:
+                    state = 1
+            #項目検索中
+            elif state == 1:
+                if line != '':
+                    key = line
+                    dictforfeedbackcsv[key] =""
+                    state = 2
+            #項目の値取得中
+            elif state == 2:
+                if line != '':
+                    dictforfeedbackcsv[key] = dictforfeedbackcsv[key] + line
+                else:
+                    key = ''
+                    value = ''
+                    state = 1
+        feedbacklist.append(dictforfeedbackcsv)
+
+import pandas as pd
+
+if iraisyolist:
+    ircsv = pd.DataFrame(iraisyolist)
+    ircsv.to_excel('iraisyo.xlsx', sheet_name='new_sheet_name',index=False, header=True)
+
+if feedbacklist:
+    fdcsv = pd.DataFrame(feedbacklist)
+    fdcsv.to_excel('feedback.xlsx', sheet_name='new_sheet_name',index=False, header=True)
+
 ######fileリストからファイルのダウンロード##################################
 import requests
 import codecs
 
-
+uniq_filelist_for_download = []
 
 for file in file_ids:
+
     channel_name = ""
     #file_ids内のチャンネルidがslack_ids_names内のチャンネルIDから見つかったら、チャンネル名を取得する
     channel_name = [cnl for cnl in slack_ids_names if cnl[1]==file['channel_id'][0]][0][0]
-
+    print(f'file_name:{file["file_name"]}, channelname:{channel_name}')
 
 
     #file_ids内のタイムスタンプからdatefoldernameを作成する
-    filedate = datetime.datetime.fromtimestamp(file['timestamp'])
+    filedate = datetime.datetime.fromtimestamp(file['timestamp'],tz=JST)
     date_folder_name = datetime.datetime(filedate.year,filedate.month,filedate.day,0,0,0,tzinfo=JST).strftime('%Y%m%d')
 
     if not box_items:
         get_items_from_box_folder(channel_folder_name=channel_name,date_folder_name=date_folder_name,root_folder_name=ROOT_FOLDER_NAME)
-        logger.info(f"not box")
+        logger.info(f"box_items is initially created")
+
     if not channel_name in box_items[ROOT_FOLDER_NAME]["items"].keys():
         get_items_from_box_folder(channel_folder_name=channel_name,date_folder_name=date_folder_name,root_folder_name=ROOT_FOLDER_NAME)
         logger.info(f'not channel {channel_name}')
+
     #date_folder_nameの存在確認を行う
     if not date_folder_name in box_items[ROOT_FOLDER_NAME]["items"][channel_name]["items"].keys():
         #存在しなければ、取得もしくは作成を行う
         get_items_from_box_folder(channel_folder_name=channel_name,date_folder_name=date_folder_name,root_folder_name=ROOT_FOLDER_NAME)
         logger.info(f'not datefolder {date_folder_name}')
+
     #ダウンロード候補ファイルの存在確認を行う
     if file['file_name'] in box_items[ROOT_FOLDER_NAME]["items"][channel_name]["items"][date_folder_name]["items"].keys():
-        logger.info(f"not file name {file['file_name']}")
+        logger.info(f"file name:{file['file_name']} was found in box_items. delete from download list. ")
         #存在している場合は削除する
-        file_ids.remove(file)
-        continue
+        # file_ids.remove(file)
+    else:
+        uniq_filelist_for_download.append[file]
+        logger.info(f"file name:{file['file_name']} is new file")
+    
 
-    #フォルダIDを取得する
-    upload_folder_id = box_items[ROOT_FOLDER_NAME]["items"][channel_name]["items"][date_folder_name]["id"]
 
-
-
+for file in uniq_filelist_for_download:
 ###################################ファイルのダウンロード##################################
     file_url = file["download_url"]
     content = requests.get(
@@ -271,29 +441,25 @@ for file in file_ids:
             ).content
     save_path = get_tmp_folder() + '/' + file["file_name"]
     
-    #既に存在したら、(1)形式でファイル名に追加
-    i = 0
-    while True:
-        i += 1
-        if os.path.exists(save_path):
-            str = save_path
-            save_path = str.split('.')[0] + f'({i}).' + str.split('.')[1]
-        else:
-            break
+    # #既に存在したら、(1)形式でファイル名に追加
+    # i = 0
+    # while True:
+    #     i += 1
+    #     if os.path.exists(save_path):
+    #         str = save_path
+    #         save_path = str.split('.')[0] + f'({i}).' + str.split('.')[1]
+    #     else:
+    #         break
 
     target_file = codecs.open(save_path, 'wb')
     target_file.write(content)
     target_file.close()
 
 ###################################ファイルのダウンロード##################################
-
-
 ###################################ファイルのアップロード##################################
+    #フォルダIDを取得する
+    upload_folder_id = box_items[ROOT_FOLDER_NAME]["items"][channel_name]["items"][date_folder_name]["id"]
     new_file = user_client.folder(upload_folder_id).upload(save_path)
     logger.info(f'File "{new_file.name}" uploaded to Box with file ID {new_file.id}')
     os.remove(save_path)
-
-    #Timeout
-    if time.time() - STARTTIME > TIMEOUT:
-        logger.info("TIMEOUT")
-        break
+    
